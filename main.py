5d6f13d244fc54b64a098d34a93734ddfcb3f9e6
Lodo Loro <69703756+6lack-L@users.noreply.github.com>
2025-07-11 09:53:14 -0230

    main

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
+def add_numbers(a, b, c):
+    """Adds two numbers and multiplies the result by a third number."""
+    #?lesson 1
+    return (a + b) * c
+def check_positive(a):
+    """Checks if a number is positive."""
+    #?lesson 2
+    return a > 0
+def print_hello_world(n):
+    """Prints 'hello world' n times."""
+    #?lesson 3
+    for i in range(n):
+        print("hello world")
+def cube_root(n):
+    """Calculates the cube root of a number."""
+    #?lesson 4
+    cube = n ** (1/3)
+    if cube % 1 == 0:
+        return int(cube)
+    else:
+        return "Error"
+
+
+
 def main():
     a = input("Enter a number: ")
     b = input("Enter another number: ")
     c = input("Enter a third number: ")
-    total = (int(a) + int(b)) * int(c)
+    total = add_numbers(int(a), int(b), int(c))
     print(f"The total is: {total}")
-    if total > 0:
+
+    if check_positive(total):
         print("The total is positive.")
-    elif total < 0:
-        print("The total is negative.")
+    elif total == 0:
+        print("The total is Zero.")
     else:
-        print("The total is zero.")
-    count = 0
+        print("The total is negative.")
+
     n = int(input("How many times to print 'hello world'? "))
-    while count < n:
-        print("hello world\n")
-        count += 1
+    print_hello_world(n)
+
+    root = cube_root(n=n)
+    print(f"The cube root of {n} is: {root}")
 if __name__ == "__main__":
     main()
