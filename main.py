1b1f8495c9815cf00f9712db46759503e1c23a63
Lodo Loro <69703756+6lack-L@users.noreply.github.com>
2025-07-23 02:28:21 -0230

    1 more for redemption

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ def even_index(str=''):
     return str[::2]
 
 def bisection_search(n=0):
-    """Performs a bisection search to find a number."""
+    """Performs a bisection(spits array in half using a double pointer......(high + low)//2 ) search to find a number."""
     #?lesson 6
     high = 1001
-    low = lst[0]
+    low = 0
     middle = (high+low)//2
     guess = 1
     if n == middle:
@@ -42,32 +42,60 @@ def bisection_search(n=0):
         guess += 1
     return middle, guess
 
+def eval_quadratic(a=0, b=0, c=0, x=0):
+    """
+    #? Lesson 7
+    a, b, c: numerical values for the coefficients of a quadratic equation
+    x: numerical value at which to evaluate the quadratic.
+    Returns the value of the quadratic a×x² + b×x + c.
+    """
+    # Your code here
+    return (a * x ** 2) + (b * x) + c
+
+
 def main():
-#    a = input("Enter a number: ")
-#    b = input("Enter another number: ")
-#    c = input("Enter a third number: ")
-#    total = add_numbers(int(a), int(b), int(c))
-#    print(f"The total is: {total}")
-#
-#    if check_positive(total):
-#        print("The total is positive.")
-#    elif total == 0:
-#        print("The total is Zero.")
-#    else:
-#        print("The total is negative.")
-#
-#    n = int(input("How many times to print 'hello world'? "))
-#    print_hello_world(n)
-#
-#    root = cube_root(n=n)
-#    print(f"The cube root of {n} is: {root}")
-#
-#    string = input("Enter a string: ")
-#    even_chars = even_index(string)
-#    print(f"Characters at even indices: {even_chars}")
-#
+#? Lesson 1
+    a = input("Enter a number: ")
+    b = input("Enter another number: ")
+    c = input("Enter a third number: ")
+    total = add_numbers(int(a), int(b), int(c))
+    print(f"The total is: {total}")
+
+#? Lesson 2
+    if check_positive(total):
+        print("The total is positive.")
+    elif total == 0:
+        print("The total is Zero.")
+    else:
+        print("The total is negative.")
+
+#? Lesson 3
+    n = int(input("How many times to print 'hello world'? "))
+    print_hello_world(n)
+
+#? Lesson 4
+    root = cube_root(n=n)
+    print(f"The cube root of {n} is: {root}")
+
+#? Lesson 5
+    string = input("Enter a string: ")
+    even_chars = even_index(string)
+    print(f"Characters at even indices: {even_chars}")
+
+#? Lesson 6
     n = int(input("search for a number: "))
     ans, guess = bisection_search(n)
-    print(f"Answer: {ans},\n Guess: {guess}")
+    print(f"Answer: {ans},\n Guess: {guess}")    
+
+#? Lesson 7
+    a = int(input("Enter coefficient 1: "))
+    b = int(input("Enter coefficient 2: "))
+    c = int(input("Enter coefficient 3: "))
+    x = int(input("Enter X variable: "))
+    ans = eval_quadratic(a,b,c,x)
+    print(ans)
+
+
+
 if __name__ == "__main__":
     main() 
